fcfb57a43fa71319f517cc33aab172db5bde55c0
OleksVov <112511040+OleksVov@users.noreply.github.com>
2023-02-25 16:41:36 +0000

    Deploying to gh-pages from @ OleksVov/goit-js-hw-09@ef5324e508a8629bf549ac3df9cfdadf16bdebad 🚀

---
+++ 03-promises.1a0b684a.js
@@ -0,0 +1,2 @@
+const e={form:document.querySelector(".form"),delay:document.querySelector('[name="delay"]'),step:document.querySelector('[name="step"]'),amount:document.querySelector('[name="amount"]')};e.form.addEventListener("submit",(t=>{t.preventDefault();Number(e.delay.value),Number(e.step.value),Number(e.amount.value)}));
+//# sourceMappingURL=03-promises.1a0b684a.js.map
---
+++ 03-promises.e315af28.js
@@ -0,0 +1,2 @@
+!function(){var e={form:document.querySelector(".form"),delay:document.querySelector('[name="delay"]'),step:document.querySelector('[name="step"]'),amount:document.querySelector('[name="amount"]')};e.form.addEventListener("submit",(function(t){t.preventDefault();Number(e.delay.value),Number(e.step.value),Number(e.amount.value)}))}();
+//# sourceMappingURL=03-promises.e315af28.js.map

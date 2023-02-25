f6ac86639e1c8c63384edb5d33402233951d77c0
Oleksandr <alex.vovchok@gmail.com>
2023-02-25 19:49:21 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -9,7 +9,6 @@ const refs = {
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
 
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
@@ -26,18 +25,20 @@ function createPromise(position, delay) {
 
 refs.form.addEventListener('submit', event => {
   event.preventDefault();
-  let delay = Number(refs.delay.value);
-  let step = Number(refs.step.value);
-  let amount = Number(refs.amount.value);
+  const delayInput = Number(refs.delay.value);
+  const step = Number(refs.step.value);
+  const amount = Number(refs.amount.value);
 
 
-
+  for ( let i = 0; i < amount; i +=1) {
+    const position = i+1;
+    const delay = delayInput + step*i;
+    createPromise(position, delay)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  }
 });
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
